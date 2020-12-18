903b916f7393ecb557bde3d6289ddcae2521f8dc
Brent Ida <idabrent14@gmail.com>
2020-12-18 13:40:52 -0800

    Set up vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import NewList from './components/NewList.vue'
+import MyRatings from './components/MyRatings.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/newList', component: NewList },
+    { path: '/myRatings', component: MyRatings }
+  ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
+
+
+
+
